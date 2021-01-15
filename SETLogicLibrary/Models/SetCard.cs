9823473c125330fc16466854b2385959430089ed
JPRchecking <john@reumerman.nl>
2021-01-15 22:05:38 +0100

    Actually moved to the logic library.

---
+++ SETLogicLibrary/Models/SetCard.cs
@@ -0,0 +1,10 @@
+﻿namespace SETLogicLibrary.Models
+{
+    public class SetCard
+    {
+        public string Shape { get; set; }
+        public string Border { get; set; }
+        public string Color { get; set; }
+        public int Count { get; set; }
+    }
+}
---
+++ SETLogicLibrary/Models/SetCardOptions.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+
+namespace SETLogicLibrary.Models
+{
+    public class SetCardOptions
+    {
+        public string[] Shapes { get; set; }
+        public string[] Borders { get; set; }
+        public string[] Colors { get; set; }
+        public int[] Counts { get; set; }
+    }
+}
