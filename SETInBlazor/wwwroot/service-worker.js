69053653c67d04aca582b778dcb056516ff8c30e
tesar-tech <33880579+tesar-tech@users.noreply.github.com>
2021-01-15 10:04:20 +0100

    Adds PWA capability

--- SETInBlazor/wwwroot/service-worker.js
+++ SETInBlazor/wwwroot/service-worker.js
@@ -1,4 +1,15 @@
 // In development, always fetch from the network and do not enable offline support.
 // This is because caching would make development more difficult (changes would not
 // be reflected on the first load after each change).
-self.addEventListener('fetch', () => { });
+//self.addEventListener('fetch', () => { });
+
+self.addEventListener('install', async event => {
+    console.log('Installing service worker...');
+    self.skipWaiting();
+});
+
+self.addEventListener('fetch', event => {
+    // You can add custom logic here for controlling whether to use cached data if offline, etc.
+    // The following line opts out, so requests go directly to the network as usual.
+    return null;
+});
