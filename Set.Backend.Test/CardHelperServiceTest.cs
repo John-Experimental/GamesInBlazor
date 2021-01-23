34edf8e9f0c351b23c80628b9e8d6c84ed7ff0c1
JPRchecking <john@reumerman.nl>
2021-01-23 21:19:28 +0100

    Added tests for verifySet

--- Set.Backend.Test/CardHelperServiceTest.cs
+++ Set.Backend.Test/CardHelperServiceTest.cs
@@ -4,6 +4,8 @@ using FluentAssertions;
 using Set.Backend.Interfaces;
 using System.Linq;
 using Set.Backend.Services;
+using Shared.Tests;
+using System.Collections.Generic;
 
 namespace Set.Backend.Test
 {
@@ -41,6 +43,154 @@ namespace Set.Backend.Test
             // If only 3 options are used, there should be 27 unique combinations, regardless of which parameter is set to false
             result.Count.Should().Be(27);
             result.Distinct().Count().Should().Be(27);
+        }
+
+        [Theory, AutoMoqData]
+        public void VerifySet_SingleParameterVariation(string[] color, string[] shape, string[] border, int[] count)
+        {
+            // This test will go through each parameter option and will see if changes in them individually maintains the set
+
+            // Different *colors*, rest remains the same
+            var card1 = CreateCard(color[0], shape[0], border[0], count[0]);
+            var card2 = CreateCard(color[1], shape[0], border[0], count[0]);
+            var card3 = CreateCard(color[2], shape[0], border[0], count[0]);
+            var setSumission = new List<SetCard> { card1, card2, card3 };
+
+            _cardHelperService.VerifySet(setSumission).Should().BeTrue();
+
+            // Different *shapes*, rest remains the same
+            card1 = CreateCard(color[0], shape[0], border[0], count[0]);
+            card2 = CreateCard(color[0], shape[1], border[0], count[0]);
+            card3 = CreateCard(color[0], shape[2], border[0], count[0]);
+            setSumission = new List<SetCard> { card1, card2, card3 };
+
+            _cardHelperService.VerifySet(setSumission).Should().BeTrue();
+
+            // Different *borders*, rest remains the same
+            card1 = CreateCard(color[0], shape[0], border[0], count[0]);
+            card2 = CreateCard(color[0], shape[0], border[1], count[0]);
+            card3 = CreateCard(color[0], shape[0], border[2], count[0]);
+            setSumission = new List<SetCard> { card1, card2, card3 };
+
+            _cardHelperService.VerifySet(setSumission).Should().BeTrue();
+
+            // Different *counts*, rest remains the same
+            card1 = CreateCard(color[0], shape[0], border[0], count[0]);
+            card2 = CreateCard(color[0], shape[0], border[0], count[1]);
+            card3 = CreateCard(color[0], shape[0], border[0], count[2]);
+            setSumission = new List<SetCard> { card1, card2, card3 };
+
+            _cardHelperService.VerifySet(setSumission).Should().BeTrue();
+        }
+
+        [Theory, AutoMoqData]
+        public void VerifySet_MultipleParameterVariation(string[] color, string[] shape, string[] border, int[] count)
+        {
+            // This test will go through each parameter option and will see if changes in them individually remains a set
+
+            // Two parameters with variations
+            var card1 = CreateCard(color[0], shape[0], border[0], count[0]);
+            var card2 = CreateCard(color[1], shape[0], border[1], count[0]);
+            var card3 = CreateCard(color[2], shape[0], border[2], count[0]);
+            var setSumission = new List<SetCard> { card1, card2, card3 };
+
+            _cardHelperService.VerifySet(setSumission).Should().BeTrue();
+
+            // Three parameters with variations and with different ordering
+            card1 = CreateCard(color[1], shape[0], border[0], count[1]);
+            card2 = CreateCard(color[2], shape[1], border[0], count[0]);
+            card3 = CreateCard(color[0], shape[2], border[0], count[2]);
+            setSumission = new List<SetCard> { card1, card2, card3 };
+
+            _cardHelperService.VerifySet(setSumission).Should().BeTrue();
+
+            // Four parameters with variation and with different ordering
+            card1 = CreateCard(color[0], shape[0], border[0], count[1]);
+            card2 = CreateCard(color[2], shape[1], border[1], count[0]);
+            card3 = CreateCard(color[1], shape[2], border[2], count[2]);
+            setSumission = new List<SetCard> { card1, card2, card3 };
+
+            _cardHelperService.VerifySet(setSumission).Should().BeTrue();
+        }
+
+        [Theory, AutoMoqData]
+        public void VerifySet_SingleFalse(string[] color, string[] shape, string[] border, int[] count)
+        {
+            // This test will go through each parameter option and will break them to test that they invalidate the set
+
+            // Different *colors*, rest remains the same
+            var card1 = CreateCard(color[0], shape[0], border[0], count[0]);
+            var card2 = CreateCard(color[1], shape[0], border[0], count[0]);
+            var card3 = CreateCard(color[1], shape[0], border[0], count[0]);
+            var setSumission = new List<SetCard> { card1, card2, card3 };
+
+            _cardHelperService.VerifySet(setSumission).Should().BeFalse();
+
+            // Different *shapes*, rest remains the same
+            card1 = CreateCard(color[0], shape[0], border[0], count[0]);
+            card2 = CreateCard(color[0], shape[0], border[0], count[0]);
+            card3 = CreateCard(color[0], shape[1], border[0], count[0]);
+            setSumission = new List<SetCard> { card1, card2, card3 };
+
+            _cardHelperService.VerifySet(setSumission).Should().BeFalse();
+
+            // Different *borders*, rest remains the same
+            card1 = CreateCard(color[0], shape[0], border[0], count[0]);
+            card2 = CreateCard(color[0], shape[0], border[1], count[0]);
+            card3 = CreateCard(color[0], shape[0], border[0], count[0]);
+            setSumission = new List<SetCard> { card1, card2, card3 };
+
+            _cardHelperService.VerifySet(setSumission).Should().BeFalse();
+
+            // Different *counts*, rest remains the same
+            card1 = CreateCard(color[0], shape[0], border[0], count[2]);
+            card2 = CreateCard(color[0], shape[0], border[0], count[1]);
+            card3 = CreateCard(color[0], shape[0], border[0], count[2]);
+            setSumission = new List<SetCard> { card1, card2, card3 };
+
+            _cardHelperService.VerifySet(setSumission).Should().BeFalse();
+        }
+
+        [Theory, AutoMoqData]
+        public void VerifySet_MultipleFalse(string[] color, string[] shape, string[] border, int[] count)
+        {
+            // This test will go through each parameter option and will break them to test that they invalidate the set
+
+            // 2 parameters false in different ways
+            var card1 = CreateCard(color[0], shape[0], border[0], count[0]);
+            var card2 = CreateCard(color[1], shape[0], border[0], count[0]);
+            var card3 = CreateCard(color[1], shape[1], border[0], count[0]);
+            var setSumission = new List<SetCard> { card1, card2, card3 };
+
+            _cardHelperService.VerifySet(setSumission).Should().BeFalse();
+
+            // 3 parameters false in the same way
+            card1 = CreateCard(color[0], shape[0], border[0], count[0]);
+            card2 = CreateCard(color[0], shape[0], border[0], count[0]);
+            card3 = CreateCard(color[1], shape[1], border[1], count[0]);
+            setSumission = new List<SetCard> { card1, card2, card3 };
+
+            _cardHelperService.VerifySet(setSumission).Should().BeFalse();
+
+            // All parameters false
+            card1 = CreateCard(color[0], shape[0], border[1], count[0]);
+            card2 = CreateCard(color[1], shape[0], border[1], count[1]);
+            card3 = CreateCard(color[0], shape[2], border[0], count[0]);
+            setSumission = new List<SetCard> { card1, card2, card3 };
+
+            _cardHelperService.VerifySet(setSumission).Should().BeFalse();
+        }
+
+
+            private SetCard CreateCard(string color, string shape, string border, int count)
+        {
+            return new SetCard
+            {
+                Color = color,
+                Shape = shape,
+                Border = border,
+                Count = count
+            };
         }
     }
 }
