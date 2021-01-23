ac938128c31388f5a5cfb7010975d2d27a24c821
JPRchecking <john@reumerman.nl>
2021-01-23 22:53:31 +0100

    Added tests for DoesFieldContainASet, both positive and negative

--- Set.Backend.Test/CardHelperServiceTest.cs
+++ Set.Backend.Test/CardHelperServiceTest.cs
@@ -181,8 +181,86 @@ namespace Set.Backend.Test
             _cardHelperService.VerifySet(setSumission).Should().BeFalse();
         }
 
+        [Theory, AutoMoqData]
+        public void DoesFieldContainASet_SingleSet(string[] color, string[] shape, string[] border, int[] count)
+        {
+            // This test will create a field with exactly one set, each separated from each other
 
-            private SetCard CreateCard(string color, string shape, string border, int count)
+            // The set
+            var card1 = CreateCard(color[0], shape[0], border[0], count[0]);
+            var card2 = CreateCard(color[1], shape[0], border[0], count[0]);
+            var card3 = CreateCard(color[2], shape[0], border[0], count[0]);
+
+            // Other cards which don't make a set
+            var card4 = CreateCard(color[1], shape[1], border[1], count[1]);
+            var card5 = CreateCard(color[1], shape[1], border[1], count[0]);
+            var card6 = CreateCard(color[1], shape[1], border[0], count[0]);
+            var card7 = CreateCard(color[1], shape[0], border[1], count[1]);
+            var card8 = CreateCard(color[1], shape[0], border[0], count[1]);
+            var card9 = CreateCard(color[2], shape[2], border[2], count[0]);
+            var card10 = CreateCard(color[2], shape[2], border[0], count[0]);
+            var card11 = CreateCard(color[2], shape[0], border[0], count[2]);
+            var card12 = CreateCard(color[1], shape[2], border[1], count[2]);
+
+            var field = new List<SetCard> { card1, card4, card5, card6, card2, card7, card8, card9, card3, card10, card11, card12 };
+
+            _cardHelperService.DoesFieldContainASet(field).Should().BeTrue();
+        }
+
+        [Theory, AutoMoqData]
+        public void DoesFieldContainASet_MultipleSets(string[] color, string[] shape, string[] border, int[] count)
+        {
+            // This test will create a field with multiple sets
+
+            // The set
+            var card1 = CreateCard(color[0], shape[0], border[0], count[0]);
+            var card2 = CreateCard(color[1], shape[0], border[0], count[0]);
+            var card3 = CreateCard(color[2], shape[0], border[0], count[0]);
+
+            // Another set, together with card1, meaning they overlap
+            var card4 = CreateCard(color[1], shape[1], border[1], count[1]);
+            var card5 = CreateCard(color[2], shape[2], border[2], count[2]);
+
+            // Cards which don't make a set
+            var card6 = CreateCard(color[1], shape[1], border[0], count[0]);
+            var card7 = CreateCard(color[1], shape[0], border[1], count[1]);
+            var card8 = CreateCard(color[1], shape[0], border[0], count[1]);
+            var card9 = CreateCard(color[2], shape[2], border[2], count[0]);
+            var card10 = CreateCard(color[2], shape[2], border[0], count[1]);
+            var card11 = CreateCard(color[2], shape[0], border[0], count[0]);
+            var card12 = CreateCard(color[1], shape[2], border[1], count[2]);
+
+            var field = new List<SetCard> { card1, card2, card4, card6, card7, card8, card9, card3, card10, card11, card12, card5 };
+
+            _cardHelperService.DoesFieldContainASet(field).Should().BeTrue();
+        }
+
+        [Theory, AutoMoqData]
+        public void DoesFieldContainASet_NoSet(string[] color, string[] shape, string[] border, int[] count)
+        {
+            // This test will create a field with no sets
+            var card1 = CreateCard(color[0], shape[0], border[0], count[0]);
+            var card2 = CreateCard(color[0], shape[0], border[0], count[1]);
+            var card3 = CreateCard(color[0], shape[0], border[1], count[0]);
+            var card4 = CreateCard(color[0], shape[1], border[0], count[0]);
+            var card5 = CreateCard(color[1], shape[0], border[0], count[0]);
+
+            var card6 = CreateCard(color[0], shape[0], border[1], count[1]);
+            var card7 = CreateCard(color[0], shape[1], border[1], count[0]);
+            var card8 = CreateCard(color[1], shape[1], border[0], count[0]);
+
+            var card9 = CreateCard(color[0], shape[1], border[1], count[1]);
+            var card10 = CreateCard(color[1], shape[1], border[1], count[0]);
+
+            var card11 = CreateCard(color[0], shape[1], border[0], count[1]);
+            var card12 = CreateCard(color[1], shape[0], border[1], count[0]);
+
+            var field = new List<SetCard> { card1, card2, card3, card4, card5, card6, card7, card8, card9, card10, card11, card12 };
+
+            _cardHelperService.DoesFieldContainASet(field).Should().BeFalse();
+        }
+
+        private SetCard CreateCard(string color, string shape, string border, int count)
         {
             return new SetCard
             {
