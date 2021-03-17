a15491df648b6ef4538af8a44360f15c4b6f79d2
JPRchecking <john@reumerman.nl>
2021-03-17 21:12:47 +0100

    Layout fixes

--- SETInBlazor/Constants/CardBackgroundColor.cs
+++ SETInBlazor/Constants/CardBackgroundColor.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-
-namespace Set.Frontend.Constants
+﻿namespace Set.Frontend.Constants
 {
     public class CardBackgroundColor
     {
