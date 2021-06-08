c5372fc72700b83d372e70579d8001bc3db35592
JPRchecking <john@reumerman.nl>
2021-06-08 21:53:54 +0200

    Attempted reset of normal service-working.js

--- SETInBlazor/wwwroot/service-worker.js
+++ SETInBlazor/wwwroot/service-worker.js
@@ -1,11 +1,4 @@
 // In development, always fetch from the network and do not enable offline support.
 // This is because caching would make development more difficult (changes would not
 // be reflected on the first load after each change).
-//self.addEventListener('fetch', () => { });
-
-self.addEventListener('install', async event => {
-    console.log('Installing service worker...');
-    self.skipWaiting();
-});
-
 self.addEventListener('fetch', () => { });
