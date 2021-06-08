fc031fe0c9cd21f660be0f7592119878f6ec1e68
JPRchecking <john@reumerman.nl>
2021-06-08 21:22:11 +0200

    Test with service working

--- SETInBlazor/wwwroot/service-worker.js
+++ SETInBlazor/wwwroot/service-worker.js
@@ -8,8 +8,4 @@ self.addEventListener('install', async event => {
     self.skipWaiting();
 });
 
-self.addEventListener('fetch', event => {
-    // You can add custom logic here for controlling whether to use cached data if offline, etc.
-    // The following line opts out, so requests go directly to the network as usual.
-    return null;
-});
+self.addEventListener('fetch', () => { });
